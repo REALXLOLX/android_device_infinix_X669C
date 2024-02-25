f74eaf1dd7dd942f04d8ae4499f5d4e4311a3f70
Hasajapathirana <80383870+Hasajapathirana@users.noreply.github.com>
2024-02-25 12:15:12 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_Infinix-X669C.mk
 
-COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X669C-user \
-    omni_Infinix-X669C-userdebug \
-    omni_Infinix-X669C-eng
+COMMON_LUNCH_CHOICES := omni_Infinix-X669C-user
